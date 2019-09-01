2cf017e1c8ebcf1a73c0e24e56fccfe2e984152e
Hossameldin Shahin <hshahin@vt.edu>
2019-09-01 14:53:35 -0400

    add support to pass metric, filter, and alarm

--- alarms.tf
+++ alarms.tf
@@ -7,65 +7,14 @@ locals {
   endpoints     = "${distinct(compact(concat(list(local.sns_topic_arn), var.additional_endpoint_arns)))}"
   region        = "${var.region == "" ? data.aws_region.current.name : var.region}"
 
-  metric_name = [
-    "AuthorizationFailureCount",
-    "S3BucketActivityEventCount",
-    "SecurityGroupEventCount",
-    "NetworkAclEventCount",
-    "GatewayEventCount",
-    "VpcEventCount",
-    "EC2InstanceEventCount",
-    "EC2LargeInstanceEventCount",
-    "CloudTrailEventCount",
-    "ConsoleSignInFailureCount",
-    "IAMPolicyEventCount",
-    "ConsoleSignInWithoutMfaCount",
-    "RootAccountUsageCount",
-    "KMSKeyPendingDeletionErrorCount",
-    "AWSConfigChangeCount",
-    "RouteTableChangesCount",
-  ]
+  metric_name = ["${var.metric_name}"]
 
   metric_namespace = "${var.metric_namespace}"
   metric_value     = "1"
 
-  filter_pattern = [
-    "{ ($.errorCode = \"*UnauthorizedOperation\") || ($.errorCode = \"AccessDenied*\") }",
-    "{ ($.eventSource = s3.amazonaws.com) && (($.eventName = PutBucketAcl) || ($.eventName = PutBucketPolicy) || ($.eventName = PutBucketCors) || ($.eventName = PutBucketLifecycle) || ($.eventName = PutBucketReplication) || ($.eventName = DeleteBucketPolicy) || ($.eventName = DeleteBucketCors) || ($.eventName = DeleteBucketLifecycle) || ($.eventName = DeleteBucketReplication)) }",
-    "{ ($.eventName = AuthorizeSecurityGroupIngress) || ($.eventName = AuthorizeSecurityGroupEgress) || ($.eventName = RevokeSecurityGroupIngress) || ($.eventName = RevokeSecurityGroupEgress) || ($.eventName = CreateSecurityGroup) || ($.eventName = DeleteSecurityGroup) }",
-    "{ ($.eventName = CreateNetworkAcl) || ($.eventName = CreateNetworkAclEntry) || ($.eventName = DeleteNetworkAcl) || ($.eventName = DeleteNetworkAclEntry) || ($.eventName = ReplaceNetworkAclEntry) || ($.eventName = ReplaceNetworkAclAssociation) }",
-    "{ ($.eventName = CreateCustomerGateway) || ($.eventName = DeleteCustomerGateway) || ($.eventName = AttachInternetGateway) || ($.eventName = CreateInternetGateway) || ($.eventName = DeleteInternetGateway) || ($.eventName = DetachInternetGateway) }",
-    "{ ($.eventName = CreateVpc) || ($.eventName = DeleteVpc) || ($.eventName = ModifyVpcAttribute) || ($.eventName = AcceptVpcPeeringConnection) || ($.eventName = CreateVpcPeeringConnection) || ($.eventName = DeleteVpcPeeringConnection) || ($.eventName = RejectVpcPeeringConnection) || ($.eventName = AttachClassicLinkVpc) || ($.eventName = DetachClassicLinkVpc) || ($.eventName = DisableVpcClassicLink) || ($.eventName = EnableVpcClassicLink) }",
-    "{ ($.eventName = RunInstances) || ($.eventName = RebootInstances) || ($.eventName = StartInstances) || ($.eventName = StopInstances) || ($.eventName = TerminateInstances) }",
-    "{ ($.eventName = RunInstances) && (($.requestParameters.instanceType = *.8xlarge) || ($.requestParameters.instanceType = *.4xlarge) || ($.requestParameters.instanceType = *.16xlarge) || ($.requestParameters.instanceType = *.10xlarge) || ($.requestParameters.instanceType = *.12xlarge) || ($.requestParameters.instanceType = *.24xlarge)) }",
-    "{ ($.eventName = CreateTrail) || ($.eventName = UpdateTrail) || ($.eventName = DeleteTrail) || ($.eventName = StartLogging) || ($.eventName = StopLogging) }",
-    "{ ($.eventName = ConsoleLogin) && ($.errorMessage = \"Failed authentication\") }",
-    "{ ($.eventName = DeleteGroupPolicy) || ($.eventName = DeleteRolePolicy) ||($.eventName=DeleteUserPolicy)||($.eventName=PutGroupPolicy)||($.eventName=PutRolePolicy)||($.eventName=PutUserPolicy)||($.eventName=CreatePolicy)||($.eventName=DeletePolicy)||($.eventName=CreatePolicyVersion)||($.eventName=DeletePolicyVersion)||($.eventName=AttachRolePolicy)||($.eventName=DetachRolePolicy)||($.eventName=AttachUserPolicy)||($.eventName=DetachUserPolicy)||($.eventName=AttachGroupPolicy)||($.eventName=DetachGroupPolicy)}",
-    "{ $.eventName = \"ConsoleLogin\" && $.additionalEventData.MFAUsed = \"No\" }",
-    "{ $.userIdentity.type = \"Root\" && $.userIdentity.invokedBy NOT EXISTS && $.eventType != \"AwsServiceEvent\" }",
-    "{ $.eventSource = kms* && $.errorMessage = \"* is pending deletion.\"}",
-    "{ $.eventSource = config.amazonaws.com && (($.eventName=StopConfigurationRecorder)||($.eventName=DeleteDeliveryChannel) ||($.eventName=PutDeliveryChannel)||($.eventName=PutConfigurationRecorder)) }",
-    "{ ($.eventName = CreateRoute) || ($.eventName = CreateRouteTable) || ($.eventName = ReplaceRoute) || ($.eventName = ReplaceRouteTableAssociation) || ($.eventName = DeleteRouteTable) || ($.eventName = DeleteRoute) || ($.eventName = DisassociateRouteTable) }",
-  ]
+  filter_pattern = ["${var.filter_pattern}"]
 
-  alarm_description = [
-    "Alarms when an unauthorized API call is made.",
-    "Alarms when an API call is made to S3 to put or delete a Bucket, Bucket Policy or Bucket ACL.",
-    "Alarms when an API call is made to create, update or delete a Security Group.",
-    "Alarms when an API call is made to create, update or delete a Network ACL.",
-    "Alarms when an API call is made to create, update or delete a Customer or Internet Gateway.",
-    "Alarms when an API call is made to create, update or delete a VPC, VPC peering connection or VPC connection to classic.",
-    "Alarms when an API call is made to create, terminate, start, stop or reboot an EC2 instance.",
-    "Alarms when an API call is made to create, terminate, start, stop or reboot a 4x-large or greater EC2 instance.",
-    "Alarms when an API call is made to create, update or delete a .cloudtrail. trail, or to start or stop logging to a trail.",
-    "Alarms when an unauthenticated API call is made to sign into the console.",
-    "Alarms when an API call is made to change an IAM policy.",
-    "Alarms when a user logs into the console without MFA.",
-    "Alarms when a root account usage is detected.",
-    "Alarms when a customer created KMS key is pending deletion.",
-    "Alarms when AWS Config changes.",
-    "Alarms when route table changes are detected.",
-  ]
+  alarm_description = ["${var.alarm_description}"]
 }
 
 resource "aws_cloudwatch_log_metric_filter" "default" {
--- variables.tf
+++ variables.tf
@@ -35,3 +35,10 @@ variable "create_dashboard" {
   description = "When true a dashboard that displays tha statistics as a line graph will be created in CloudWatch"
   default     = "true"
 }
+
+variable "metric_name" {}
+
+variable "filter_pattern" {}
+
+variable "alarm_description" {}
+
